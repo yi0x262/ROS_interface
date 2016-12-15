8a2f48914c980ab7450f82721bb31e428c83b95d
yi0x262 <yutadefn@gmail.com>
2016-12-15 14:35:16 +0900

    anyway

---
+++ service_caller.py
@@ -0,0 +1,6 @@
+import rospy
+import std_srvs.srv
+
+class service_caller(rospy.ServiceProxy):
+    def __new__(self,service_name,service_type=std_srvs.srv._Empty.Empty):
+        return super().__new__(service_name,service_type)
--- simlation_manager.py
+++ simlation_manager.py
@@ -1,4 +1,20 @@
 #!/usr/env/bin python3
 
+from list_pub_sub import list_publisher,list_subscriber
+from clock import clock_manager
+
+from gazebo_msgs.msg import LinkStates
+from control_msgs.msg import JointControllerState
+
+        
+
 class simulation_manager(object):
-    
+    def __init__(self,joint_names):
+        self.delta_time = clock_manager()
+
+        self.joint_states = list_subscriber([name+'/state' for name in joint_names],JointControllerState)
+        self.joint_commands = list_publisher([name+'/command' for name in joint_names])
+        self.link_states =
+    def __call__(self,action):
+        pass
+    def reset(self):
