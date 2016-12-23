9990a1b82d5cb7ccc106a8795bf5cc6295d56747
yi0x262 <yutadefn@gmail.com>
2016-12-24 07:42:43 +0900

    anyway

--- subscribers.py
+++ subscribers.py
@@ -1,57 +1,35 @@
-#all class's get_msg() return not nested list
+#all class's get_msg() return not-nested list
+from InfoGetter import InfoGetter
 
-
-class state_getter(InfoGetter):
-    def __init__(self,msgname,msgtype):
-        super().__init__(self)
-        rospy.Subscriber(msgname,msgtype,self)
-
-from gazebo_msgs.msg import LinkStates
-class link_states(state_getter):
+from control_msgs.msg import JointControllerState
+class JointState_getter(InfoGetter):
     def __init__(self,msgname):
-        super().__init__(self,msgname,LinkStates)
-    def extend_xyz(self,*args):
-        ret = list()
-        for data in args:
-            for d in data:
-                ret.extend(d.x,d.y,d.z)
-        return ret
+        super().__init__(self,msgname,JointControllerState)
     def get_msg(self):
-        data = super().get_msg()
-        #!!!not done!!!
+        pass
 
-from sensor_msgs.msg import JointState
-class joint_states(state_getter):
+class imu_states(InfoGetter):
     def __init__(self,msgname):
-        super().__init__(self,msgname,JointState)
+        super().__init__(self,msgname,)
     def get_msg(self):
-        data = super().get_msg()
-        ret = list()
-        ret.extend(data.position)
-        ret.extend(data.velocity)
-        ret.extend(data.effort)
-        return ret
 
 class list_subscriber(list):
-    def __init__(self,msgnames,classnames):
-        if not isinstance(msgtype,(list,tuple)):
-            msgtypes = [msgtype for _ in msgnames]
-        else:
-            msgtypes = msgtype
-        for name,mt in zip(msgnames,msgtypes):
-            self.append(async_subscriber(name,mt))
+    def __new__(self,msgnames,info_class):
+        return [info_class(name) for name in msgnames]
 
     def get_msg(self):
         data = list()
         for sub in self:
             d = sub.get_msg()
-            if d is None:
-                raise RuntimeError('in list_subscriber : yet receive msg')
+            assert d is None, RuntimeError('in list_subscriber : yet receive msg')
             data.append(d)
         return data
     def reset(self):
         for i in range(len(self)):
             self[i].set_None()
 
-class imu_states(list_subscriber):
+class ImuState_list(list_subscriber):
+    def __new__(self,msgnames):
+        return super().__new__(msgnames,imu_states)
     def get_msg(self):
+        pass
